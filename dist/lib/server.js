554ace7312101f820ad029e53c0d222af6d472ff
unknown <sapek.arkadiusz@gmail.com>
2018-08-28 01:06:26 +0200

    changed start script and tsc build path

--- dist/lib/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-app_1.default.get('/', (req, res) => res.send('React-Form api is online!'));
-app_1.default.listen(process.env.PORT || 4000, function () {
-    console.log('now listening for requests');
-});
-//# sourceMappingURL=server.js.map
