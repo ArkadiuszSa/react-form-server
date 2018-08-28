788948b33cd577a95762aec5587bc3bf9b69e3a5
unknown <sapek.arkadiusz@gmail.com>
2018-08-28 22:19:13 +0200

    changed env condition for db

--- lib/database-config.ts
+++ lib/database-config.ts
@@ -5,13 +5,16 @@ export default class DatabaseConfig {
   private mongoUrl: string;
 
   constructor() {
-    if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "dev") {
+    if (
+      process.env.NODE_ENV === "production" ||
+      process.env.NODE_ENV === "dev"
+    ) {
       this.mongoSetup();
     }
   }
 
   private mongoSetup(): void {
-    if (process.env.NODE_ENV === "prod") {
+    if (process.env.NODE_ENV === "production") {
       this.mongoUrl = process.env.CONNECTION_STRING_PROD;
     } else if (process.env.NODE_ENV === "dev") {
       this.mongoUrl = process.env.CONNECTION_STRING_DEV;
